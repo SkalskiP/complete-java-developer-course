aa228ade96b6ffb590b6ae814cbace4d40a56d68
SkalskiP <pskalski@student.agh.edu.pl>
2017-10-11 00:18:02 +0200

    basic numbers types

--- 04_Variables_Datatypes_and_Operators/ByteShortInt/src/com/piotrskalski/Main.java
+++ 04_Variables_Datatypes_and_Operators/ByteShortInt/src/com/piotrskalski/Main.java
@@ -3,6 +3,32 @@ package com.piotrskalski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+        System.out.println("longTotal = " + longTotal);
+        System.out.println("shortTotal = " + shortTotal);
+
+        /*// int has a width of 32
+        int myMinValue = - 2_147_483_648;
+        int myMaxValue = 2_147_483_647;
+        int myTotal = (myMinValue/2);
+        System.out.println("myTotal = " + myTotal);
+
+        // byte has a width of 8
+        byte myByteValue = -128;
+        byte myNewByteValue = (byte) (myByteValue/2);
+        System.out.println("myNewByteValue = " + myByteValue);
+
+        // short has a width of 16
+        short myShortValue = -32767;
+        short myNewShortValue = (short) (myShortValue/2);
+
+        // long has a width of 64
+        long myLongValue = 9_223_372_036_854_755_807L;*/
     }
 }
