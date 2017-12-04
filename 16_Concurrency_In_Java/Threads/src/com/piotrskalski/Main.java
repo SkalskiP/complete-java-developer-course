e9514ef4cdc36d21bb422f7efeb90fe40a6fc5d6
SkalskiP <pskalski@student.agh.edu.pl>
2017-12-04 22:50:54 +0100

    1 part of threads done

--- 16_Concurrency_In_Java/Threads/src/com/piotrskalski/Main.java
+++ 16_Concurrency_In_Java/Threads/src/com/piotrskalski/Main.java
@@ -2,6 +2,7 @@ package com.piotrskalski;
 
 import static com.piotrskalski.ThreadColor.ANSI_GREEN;
 import static com.piotrskalski.ThreadColor.ANSI_PURPLE;
+import static com.piotrskalski.ThreadColor.ANSI_RED;
 
 public class Main {
 
@@ -9,6 +10,7 @@ public class Main {
         System.out.println(ANSI_PURPLE + "Hello from the main thread.");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
 
         new Thread() {
@@ -16,6 +18,23 @@ public class Main {
                 System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
             }
         }.start();
+
+        Thread myRunnableThread = new Thread(new MyRunnable() {
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted.");
+                }
+            }
+        });
+
+        myRunnableThread.start();
+//        anotherThread.interrupt();
+
 
         System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
 
---
+++ 16_Concurrency_In_Java/Threads/src/com/piotrskalski/MyRunnable.java
@@ -0,0 +1,12 @@
+package com.piotrskalski;
+
+import static com.piotrskalski.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
+    }
+
+}
