328f3e7b47edd65fcbdf7d5172e791f0ba108c51
SkalskiP <pskalski@student.agh.edu.pl>
2018-01-04 23:05:49 +0100

    Constructiors

--- 07_OOP_Part_1/Constructors/src/com/piotrskalski/Main.java
+++ 07_OOP_Part_1/Constructors/src/com/piotrskalski/Main.java
@@ -3,6 +3,18 @@ package com.piotrskalski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//        Account bobsAccount = new Account("12345", 0.00, "Bob Brown", "myemail@bob.com", "(087) 123-4567");
+        Account bobsAccount = new Account();
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(50.0);
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(51.0);
+        bobsAccount.withdrawal(100.0);
     }
 }
