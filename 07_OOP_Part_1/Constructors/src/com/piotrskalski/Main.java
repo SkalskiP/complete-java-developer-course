be900f7832a9edf987c944c5385eec2c9d688212
SkalskiP <pskalski@student.agh.edu.pl>
2018-01-05 23:14:36 +0100

    Inharitance

--- 07_OOP_Part_1/Constructors/src/com/piotrskalski/Main.java
+++ 07_OOP_Part_1/Constructors/src/com/piotrskalski/Main.java
@@ -5,16 +5,28 @@ public class Main {
     public static void main(String[] args) {
 
 //        Account bobsAccount = new Account("12345", 0.00, "Bob Brown", "myemail@bob.com", "(087) 123-4567");
-        Account bobsAccount = new Account();
-        System.out.println(bobsAccount.getNumber());
-        System.out.println(bobsAccount.getBalance());
+//        Account bobsAccount = new Account();
+//        System.out.println(bobsAccount.getNumber());
+//        System.out.println(bobsAccount.getBalance());
+//
+//        bobsAccount.withdrawal(100.0);
+//
+//        bobsAccount.deposit(50.0);
+//        bobsAccount.withdrawal(100.0);
+//
+//        bobsAccount.deposit(51.0);
+//        bobsAccount.withdrawal(100.0);
+//
+//        Account timsAccount = new Account("Tim", "tim@email.com", "12345");
 
-        bobsAccount.withdrawal(100.0);
+        VipPerson person1 = new VipPerson();
+        System.out.println(person1.getName());
 
-        bobsAccount.deposit(50.0);
-        bobsAccount.withdrawal(100.0);
+        VipPerson person2 = new VipPerson("Bob", 25000);
+        System.out.println(person2.getName());
 
-        bobsAccount.deposit(51.0);
-        bobsAccount.withdrawal(100.0);
+        VipPerson person3 = new VipPerson("Tim", 100.00, "tim@email.com");
+        System.out.println(person3.getName());
+
     }
 }
